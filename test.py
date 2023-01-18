060e8bfa43f57db9570dce188f2c5b452d21c570
iwantmymtv <PTO@vestfrostsolutions.hu>
2023-01-18 16:48:20 +0100

    aa

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import sys
 from pymilvus import connections
 from pymilvus import Collection
+from decouple import config
 
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-milvus_uri = 
-user = 
-password = 
+milvus_uri = config("MILVUS_URI")
+user = config("MILVUS_USER")
+password = config("MILVUS_PSWD")
+
+connections.connect("default",
+                    uri=milvus_uri,
+                    user=user,
+                    password=password,
+                    secure=True)
+                    
 #Our sentences we like to encode
 sentences = ['Orbán kijelölte a magyar középhatalmiság útját, ami zsibbasztó közhelyekkel van kikövezve',
     'Rendkívüli szünetet rendeltek el abban a szentgotthárdi iskolában, ahol 29 tanár kezdett polgári engedetlenségbe',
